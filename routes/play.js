4d52d51657e6ac1cf746170d4b5d24fb35086f58
yooseok Seo <yos017@ucsd.edu>
2018-02-10 03:11:15 -0800

    clean up - new starter code

--- routes/play.js
+++ routes/play.js
@@ -1,7 +1,7 @@
 var data = require('../data.json');
 
 exports.view = function(req, res){
-  var pageTitle = req.params.abc;
+  var pageTitle = req.params.categoryTitle;
   console.log(pageTitle);
   res.render('play', {
   	'pageTitle': pageTitle
