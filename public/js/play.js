f74a1418298b99f539759f70afeb56286b5262ed
Peem Takoonsawat <mrpeem@gmail.com>
2018-02-11 00:01:02 -0800

    login/signup; copied from online

--- public/js/play.js
+++ public/js/play.js
@@ -2,11 +2,9 @@
 
 $(document).ready(function() {
 
-$('#moreBtn').click(function() {
-  $('#page-media-title').toggle();
-  $('#page-madia-caption').toggle("slow");
-});
-
-
+	$('#moreBtn').click(function() {
+	  $('#page-media-title').toggle();
+	  $('#page-madia-caption').toggle("slow");
+	});
 
 })
