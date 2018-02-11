c651c21e07c8b8d03a1c0ef7add511ed3d129598
Peem Takoonsawat <mrpeem@gmail.com>
2018-02-11 01:37:19 -0800

    remove test file

--- app.js
+++ app.js
@@ -45,6 +45,6 @@ app.get('/play/:categoryTitle/:itemId/info/:web', web.view);
 // Example route
 // app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.listen(3000, function () {
+console.log("Express server listening on port 3000");
 });
